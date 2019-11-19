112ad2c25259c13aa9a0f26ed885c742ca7e86a1
alfatta <arwinnersyah@gmail.com>
2019-11-19 21:16:00 +0700

    Add insert tech stacks

--- routes.js
+++ routes.js
@@ -5,5 +5,6 @@ const techStacksController = require('./controller/techStacks')
 const Router = express.Router()
 
 Router.get('/tech-stacks', techStacksController.getAll)
+Router.post('/tech-stacks', techStacksController.insert)
 
 module.exports = Router
